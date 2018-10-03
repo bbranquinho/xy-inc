04a96056fba3b8b070a5dfaa96ea1497d996ba64
Augusto A. B. Branquinho <augustobranquinho@gmail.com>
2018-10-03 10:21:44 -0500

    Update kotlin and spring.

--- xy-inc-api/xy-inc-utils/src/main/kotlin/br/com/xy/inc/utils/ApplicationProperties.kt
+++ xy-inc-api/xy-inc-utils/src/main/kotlin/br/com/xy/inc/utils/ApplicationProperties.kt
@@ -7,10 +7,10 @@ import org.springframework.stereotype.Component
 open class ApplicationProperties {
 
     @Value("\${app.project.path}")
-    var projectPath: String? = null
+    lateinit var projectPath: String
 
-    @Value("\${app.api.log.size:#{null}}")
-    var apiLogSize: Int? = null
+    @Value("\${app.api.log.size:#{0}}")
+    var apiLogSize: Int = 0
 
     @Value("\${app.api.start.automatically:#{false}}")
     var isStartApiAutomatically: Boolean = false
--- xy-inc-api/xy-inc-utils/src/main/kotlin/br/com/xy/inc/utils/JacksonExtension.kt
+++ xy-inc-api/xy-inc-utils/src/main/kotlin/br/com/xy/inc/utils/JacksonExtension.kt
@@ -3,21 +3,17 @@ package br.com.xy.inc.utils
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import java.io.File
 
-private object JacksonExtension {
-
-    val jacksonObjectMapper = jacksonObjectMapper()
-
-}
+private val jacksonObjectMapper = jacksonObjectMapper()
 
 fun File.writeJsonToFile(obj: Any) =
-        JacksonExtension.jacksonObjectMapper.writerWithDefaultPrettyPrinter().writeValue(this, obj)
+        jacksonObjectMapper.writerWithDefaultPrettyPrinter().writeValue(this, obj)
 
 fun <T> File.jsonToObject(t: Class<T>): T =
-        JacksonExtension.jacksonObjectMapper.readValue(this, t)
+        jacksonObjectMapper.readValue(this, t)
 
 fun <T> String.jsonToObject(t: Class<T>): T =
-        JacksonExtension.jacksonObjectMapper.readValue(this, t)
+        jacksonObjectMapper.readValue(this, t)
 
 fun <T> T.objectToJson(): String =
-        JacksonExtension.jacksonObjectMapper.writeValueAsString(this)
+        jacksonObjectMapper.writeValueAsString(this)
 
